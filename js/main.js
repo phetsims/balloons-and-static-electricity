c55edd12df32cee952d583915b608a0888c3088e
Sam Reid <reids@colorado.edu>
2013-04-16 12:37:31 -0600

    added test usage of fort and scenery to make sure they are loading properly

--- js/main.js
+++ js/main.js
@@ -8,12 +8,22 @@
  */
 require( [
            'fastclick',
-           'PHETCOMMON/util/ImagesLoader'
-         ], function( FastClick, ImagesLoader ) {
+           'PHETCOMMON/util/ImagesLoader',
+           'FORT/Fort',
+           'SCENERY/nodes/Text'
+         ], function( FastClick, ImagesLoader, Fort, Text ) {
   'use strict';
 
   //On iPad, prevent buttons from flickering 300ms after press.  See https://github.com/twitter/bootstrap/issues/3772
   new FastClick( document.body );
 
+  //Make sure Fort loaded
   console.log( "started" );
+  var Model = Fort.Model.extend( {paused: true} );
+  var model = new Model();
+  console.log( model );
+
+  //make sure scenery loaded
+  var text = new Text( "hello" );
+  console.log( text );
 } );
