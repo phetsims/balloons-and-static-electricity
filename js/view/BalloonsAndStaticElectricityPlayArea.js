df534c511d0726cb6c5fb11b6d144556f52703b0
Sam Reid <reids@colorado.edu>
2013-06-14 14:51:01 -0600

    Changed z-ordering for navbar extension, see #54

--- js/view/BalloonsAndStaticElectricityPlayArea.js
+++ js/view/BalloonsAndStaticElectricityPlayArea.js
@@ -12,6 +12,9 @@ define( function( require ) {
   function BalloonsAndStaticElectricityPlayArea( model ) {
     TabView.call( this );
 
+    //A black rectangle that vertically 'extends' the navbar from joist, see #54
+    this.addChild( new Rectangle( 0, 0, 3000, this.layoutBounds.height, {fill: 'black', x: -1000, y: this.layoutBounds.height, pickable: false} ) );
+
     this.addChild( new SweaterNode( model ) );
 
     var wall = new WallNode( model );
@@ -19,9 +22,6 @@ define( function( require ) {
 
     this.addChild( new BalloonNode( 500, 200, model.balloons[1], "images/balloon-green.png", model ) );
     this.addChild( new BalloonNode( 400, 200, model.balloons[0], "images/balloon-yellow.png", model ) );
-
-    //A black rectangle that vertically 'extends' the navbar from joist, see #54
-    this.addChild( new Rectangle( 0, 0, 3000, this.layoutBounds.height, {fill: 'black', x: -1000, y: this.layoutBounds.height, pickable: false} ) );
 
     this.addChild( new ControlPanel( Strings, model ) );
   }
