372e092a1cd8b361f2c4235bcc6069c01dd46dc1
samreid <reids@colorado.edu>
2013-08-08 21:33:18 -0600

    Changed the size of the reset all button to match Beer's Law Lab

--- js/view/ControlPanel.js
+++ js/view/ControlPanel.js
@@ -73,7 +73,11 @@ define( function( require ) {
 
     //Link plural vs singular afterwards so the button layout will accommodate both
     model.balloons[1].isVisibleProperty.link( function( both ) {resetBalloonText.text = strings[both ? "BalloonApplet.resetBalloons" : "BalloonApplet.resetBalloon"];} );
-    var controls = new HBox( {spacing: 16, align: 'bottom', children: [new ResetAllButton( model.reset.bind( model ) ), wallButton]} );
+
+    //Add the controls at the right, with the reset all button and the wall button
+    //The reset all button is scaled to match the size in Beer's Law Lab (which is also scaled because of the TabView/ScreenView)
+    //TODO: remove the reset all button scale factor after the ResetAllButton is specifying the correct size itself, after Phase I deploys
+    var controls = new HBox( {spacing: 16, align: 'bottom', children: [new ResetAllButton( model.reset.bind( model ), {scale: 88 / 121} ), wallButton]} );
 
     controls.right = layoutBounds.maxX - 2;
     controls.bottom = layoutBounds.maxY - 4;
