117e4b3cc19edb4a5b30a95510a306f5c8cdb80e
Chris Malley <cmalley@pixelzoom.com>
2015-01-16 16:33:12 -0700

    clean up config.js, it was a mess

--- js/balloons-and-static-electricity-config.js
+++ js/balloons-and-static-electricity-config.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2013, University of Colorado Boulder
+// Copyright 2002-2015, University of Colorado Boulder
 
 /**
  * RequireJS configuration file for the sim.
@@ -7,35 +7,32 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 require.config( {
-  // An array of dependencies to load. Useful when require is defined as a config object before require.js
-  // is loaded, and you want to specify dependencies to load as soon as require() is defined.
+
   deps: [ 'balloons-and-static-electricity-main' ],
 
-  // baseUrl: don't bother trying to set it here, it is overridden by data-main in the top-level HTML file
-
-  // Path mappings for module names not found directly under baseUrl. The path settings are assumed to be
-  // relative to baseUrl unless the paths setting starts with a '/' or has a URL protocol.
   paths: {
-    AXON: '../../axon/js',
-    BRAND: '../../brand/js',
 
-    // common directories, uppercase names to identify them in require imports
-    PHETCOMMON: '../../phetcommon/js',
-    SCENERY: '../../scenery/js',
-    SCENERY_PHET: '../../scenery-phet/js',
-    KITE: '../../kite/js',
-    PHET_CORE: '../../phet-core/js',
-    DOT: '../../dot/js',
-    SUN: '../../sun/js',
-    JOIST: '../../joist/js',
-    BALLOONS_AND_STATIC_ELECTRICITY: '.',
-
-    // local contrib dependencies
+    // plugins
     image: '../../chipper/requirejs-plugins/image',
     audio: '../../chipper/requirejs-plugins/audio',
     string: '../../chipper/requirejs-plugins/string',
 
-    text: '../../sherpa/text'
+    text: '../../sherpa/text',
+
+    // PhET libs, uppercase names to identify them in require.js imports
+    AXON: '../../axon/js',
+    BRAND: '../../brand/js',
+    DOT: '../../dot/js',
+    JOIST: '../../joist/js',
+    KITE: '../../kite/js',
+    PHET_CORE: '../../phet-core/js',
+    PHETCOMMON: '../../phetcommon/js',
+    SCENERY: '../../scenery/js',
+    SCENERY_PHET: '../../scenery-phet/js',
+    SUN: '../../sun/js',
+
+    // sim code
+    BALLOONS_AND_STATIC_ELECTRICITY: '.'
   },
 
   urlArgs: new Date().getTime()  // cache buster to make browser reload all included scripts
