0e50914d64581c7ce61f1c3557e5def815ec79a5
Chris Malley <cmalley@pixelzoom.com>
2015-02-11 18:39:21 -0700

    move requirejs plugins to chipper/js/requirejs-plugins, phetsims/chipper#92

--- js/balloons-and-static-electricity-config.js
+++ js/balloons-and-static-electricity-config.js
@@ -13,9 +13,9 @@ require.config( {
   paths: {
 
     // plugins
-    image: '../../chipper/requirejs-plugins/image',
-    audio: '../../chipper/requirejs-plugins/audio',
-    string: '../../chipper/requirejs-plugins/string',
+    image: '../../chipper/js/requirejs-plugins/image',
+    audio: '../../chipper/js/requirejs-plugins/audio',
+    string: '../../chipper/js/requirejs-plugins/string',
 
     text: '../../sherpa/text',
 
