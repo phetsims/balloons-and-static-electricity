4476fdd2e05ab5a001b194534f9199ea408edb76
Jesse <jesse.greenberg@colorado.edu>
2015-12-03 18:51:19 -0700

    Added accessible header with labels and descriptions

--- js/balloons-and-static-electricity/view/BalloonsAndStaticElectricityView.js
+++ js/balloons-and-static-electricity/view/BalloonsAndStaticElectricityView.js
@@ -17,6 +17,8 @@ define( function( require ) {
   // strings
   var yellowBalloonDescriptionString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/yellowBalloon.description' );
   var greenBalloonDescriptionString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/greenBalloon.description' );
+  var screenViewLabelString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/screenView.label');
+  var screenViewDescriptionString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/screenView.description');
 
   // images
   var balloonGreen = require( 'image!BALLOONS_AND_STATIC_ELECTRICITY/balloon-green.png' );
@@ -38,7 +40,7 @@ define( function( require ) {
     this.addChild( new Rectangle( maxX - 1000, 0, 1000, 1000, { fill: 'black' } ) );
 
     var balloonsNode = new Node();
-    var greenBalloon = new BalloonNode( 500, 200, model.balloons[ 1 ], balloonGreen, model, { accessibleDescription: greenBalloonDescriptionString } );
+    var greenBalloon = new BalloonNode( 500, 200, model.balloons[ 1 ], balloonGreen, model, { zibleDescription: greenBalloonDescriptionString } );
     var yellowBalloon = new BalloonNode( 400, 200, model.balloons[ 0 ], balloonYellow, model, { accessibleDescription: yellowBalloonDescriptionString } );
     balloonsNode.children = [ greenBalloon, yellowBalloon ];
     this.addChild( balloonsNode );
@@ -57,6 +59,56 @@ define( function( require ) {
       y: this.layoutBounds.height,
       pickable: false
     } ) );
+
+    // outfit a11y
+    this.accessibleContent = {
+      createPeer: function( accessibleInstance ) {
+
+        // generate the 'supertype peer' for the ScreenView in the parallel DOM.
+        var accessiblePeer = ScreenView.ScreenViewAccessiblePeer( accessibleInstance );
+        var domElement = accessiblePeer.domElement;
+
+        // we want the parallel DOM to look like this:
+        // <header role="banner" aria-labelledby="scene-label" aria-describedby="scene-description">
+        //   <h1 id="scene-label">The Scene for Balloons &amp; Static Electricity</h1> 
+        //   <!-- General scene description for page load. Parts will need change dynamically once I figure that out.-->
+        //   <div id="scene-description">
+        //     <p>A balloon floats evenly between a large woolly sweater and a removable wall. The sweater is to the left and the wall is to the right.</p> 
+        //     <p>Each object shows negative and positive charges. Charges are shown in pairs. Currently, the sweater and the wall have many pairs of charges, the balloon, just a few.</p>
+        //     <p>The control panel has controls to add a second balloon, adjust charge views and to reset the entire experiment.</p>
+        //     <p>Select Tab for next item. Select K or question mark for keyboard help. Select H for a heading outline.</p>
+        //   </div>
+        // </header>
+
+        var headerElement = document.createElement( 'header' );
+        headerElement.setAttribute( 'role', 'banner' );
+        headerElement.setAttribute( 'aria-labelledby', 'scene-label' );
+        headerElement.setAttribute( 'aria-describedby', 'scene-description' );
+
+        // create the scene label
+        var labelElement = document.createElement( 'h1' );
+        labelElement.id = 'scene-label';
+        labelElement.innerText = screenViewLabelString;
+
+        // general scene descriptoin for page load.  Parts of the description will need to change dynamically.
+        var descriptionElement = document.createElement( 'div' );
+        descriptionElement.id = 'scene-description';
+
+        // TODO: Why is there a separate paragraph(s)  for this?  Could we skip the div and just go right into the paragraph for the descriptoin?
+        var descriptionParagraph = document.createElement( 'p' );
+        descriptionParagraph.innerText = screenViewDescriptionString;
+
+        // structure these elements
+        domElement.appendChild( headerElement );
+        headerElement.appendChild( labelElement );
+        headerElement.appendChild( descriptionElement );
+        descriptionElement.appendChild( descriptionParagraph );
+
+        // var accessiblePeer = new AccessiblePeer( accessibleInstance, headerElement );
+
+        return accessiblePeer;
+      }
+    };
 
     // define the accessible order for this screen
     this.accessibleOrder = [ yellowBalloon, greenBalloon, wall ];
