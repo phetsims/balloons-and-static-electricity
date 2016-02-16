d959139d4107bb6290574c2e0538fbd35e2e131f
Jesse Greenberg <jesse.greenberg@colorado.edu>
2016-02-16 16:43:37 -0700

    Create accessible div type, node acting as a container for other accessible peers, see #121

---
+++ js/balloons-and-static-electricity/accessibility/AccessibleDivNode.js
@@ -0,0 +1,47 @@
+// Copyright 2015, University of Colorado Boulder
+
+/**
+ * A Scenery node used to contain a div element in the Parallel DOM.  By giving the element its own node, we can 
+ * contain it and have full control of its location in the parallel DOM relative to other child elements.
+ * 
+ * This node is entirely invisible, other than its representation in the accessibility tree.
+ * 
+ * @author: Jesse Greenberg
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var Node = require( 'SCENERY/nodes/Node' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var AccessiblePeer = require( 'SCENERY/accessibility/AccessiblePeer' );
+
+  /**
+   * Create a node that contains a heading so that users can use AT to quickly find content in the DOM
+   * 
+   * @constructor
+   **/
+  function AccessibleDivNode() {
+
+    var thisNode = this;
+
+    Node.call( this, {
+      accessibleContent: {
+        createPeer: function( accessibleInstance ) {
+          var trail = accessibleInstance.trail;
+          this.node = trail.lastNode(); // @public (a11y)
+
+          // heading element
+          var domElement = document.createElement( 'div' );
+          domElement.id = 'element-container-' + this.node.id;
+          thisNode.accessibleId = domElement.id;
+
+          return new AccessiblePeer( accessibleInstance, domElement );
+        }
+      }
+    } );
+  }
+
+  return inherit( Node, AccessibleDivNode );
+
+} );
