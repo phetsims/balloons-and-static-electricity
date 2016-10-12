0e4fb507491fd8d170f98aa85f171adb84cce06d
Jesse Greenberg <jesse.greenberg@colorado.edu>
2016-10-12 16:45:47 -0600

    New sweater descriptions, see #154

--- js/balloons-and-static-electricity/view/SweaterNode.js
+++ js/balloons-and-static-electricity/view/SweaterNode.js
@@ -11,41 +11,48 @@ define( function( require ) {
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
+  var AccessibleNode = require( 'BALLOONS_AND_STATIC_ELECTRICITY/balloons-and-static-electricity/accessibility/AccessibleNode' );
   var Image = require( 'SCENERY/nodes/Image' );
   var PlusChargeNode = require( 'BALLOONS_AND_STATIC_ELECTRICITY/balloons-and-static-electricity/view/PlusChargeNode' );
   var MinusChargeNode = require( 'BALLOONS_AND_STATIC_ELECTRICITY/balloons-and-static-electricity/view/MinusChargeNode' );
-  var AccessiblePeer = require( 'SCENERY/accessibility/AccessiblePeer' );
   var StringUtils = require( 'PHETCOMMON/util/StringUtils' );
+  var Range = require( 'DOT/Range' );
+  var balloonsAndStaticElectricity = require( 'BALLOONS_AND_STATIC_ELECTRICITY/balloonsAndStaticElectricity' );
+
+  // constants - ranges to describe charges in the sweater
+  var A_FEW_RANGE = new Range( 0, 15 );
+  var SEVERAL_RANGE = new Range( 15, 40 );
+  var MANY_RANGE = new Range( 40, 56 );
+  var MAX_CHARGE = 57;
 
   // strings
   var sweaterLabelString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/sweater.label' );
-  var sweaterDescriptionPatternString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/sweater.descriptionPattern' );
-  var neutralString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/neutral' );
-  var netPositiveString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/netPositive' );
-  var noString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/no' );
-  var aFewString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/aFew' );
-  var severalString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/several' );
-  var manyString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/many' );
-  var sweaterNoMoreChargesString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/sweater.noMoreCharges' );
-  var balloonsAndStaticElectricity = require( 'BALLOONS_AND_STATIC_ELECTRICITY/balloonsAndStaticElectricity' );
+  var sweaterDescriptionPatternString = 'Sweater has net {0} charge, {1} more positive charges than negative charges';
+  var sweaterChargeDepletedString = 'Sweater has a net positive charge, no negative charges, only positive charges';
+
+  var neutralString = 'neutral';
+  var positiveString = 'positive';
+
+  var noString = 'no';
+  var aFewString = 'a few';
+  var severalString = 'several';
+  var manyString = 'many';
 
   // images
   var sweater = require( 'image!BALLOONS_AND_STATIC_ELECTRICITY/sweater.jpg' );
-
-  // // a map to get the correct charge description for the sweater - based on the net model charge
-  // var chargeAmountDescriptionMap = {
-  //   0: noString,
-  //   5: aFewString,
-  //   25: severalString,
-  //   45: manyString
-  // };
-  //
 
   function SweaterNode( model ) {
     var self = this;
 
     // super constructor
-    Node.call( this, { pickable: false } );
+    AccessibleNode.call( this, null, {
+      pickable: false,
+
+      // accesssibility options
+      tagName: 'div', // sweater is just a div
+      labelTagName: 'h3', // label is identified as a heading of level 3
+      label: sweaterLabelString
+    } );
 
     this.plusChargesNode = new Node();
     this.minusChargesNode = new Node( { layerSplit: true } );
@@ -95,86 +102,53 @@ define( function( require ) {
       updateChargesVisibilityOnSweater( value );
     } );
 
-    this.sweaterModel.chargeProperty.link( function() {
+    this.sweaterModel.chargeProperty.link( function( charge ) {
       updateChargesVisibilityOnSweater( model.showCharges );
+
+      // a11y - update description of sweater when charge changes
+      var chargeDescription = self.getChargeDescription( charge );
+      self.setDescription( chargeDescription );
+      console.log( chargeDescription );
     } );
-
-    // outfit a11y
-    this.accessibleContent = {
-      createPeer: function( accessibleInstance ) {
-        var trail = accessibleInstance.trail;
-        var uniqueId = trail.getUniqueId();
-
-        // the parallel DOM element should look like this:
-        // <!-- Sweater is not in the tab order, but needs to be a live region, so changes in charges are communicated. -->
-        //  <div id="sweater-widget" aria-labelledby="sweater-label" aria-describedby="balloon-description">
-        //    <h3 id="sweater-label">Sweater</h3>
-        //    <!-- Sweater charge information changes and will need to be associated with the balloon. -->
-        //    <p id="sweater-description">The sweater has a neutral charge, no more positive charges than negative ones.</p>
-        //  </div>
-
-        // create the div element and assign it a unique id.
-        var domElement = document.createElement( 'div' );
-        domElement.id = 'sweater-' + uniqueId;
-        domElement.setAttribute( 'aria-live', 'polite' );
-
-        // create the label element, and assign it as an aria label for the above div
-        var labelElement = document.createElement( 'h3' );
-        labelElement.textContent = sweaterLabelString;
-        labelElement.id = 'sweater-label-' + uniqueId;
-        domElement.setAttribute( 'aria-labelledby', labelElement.id );
-
-        // create the description element and assign it a unique id.
-        var descriptionElement = document.createElement( 'p' );
-        descriptionElement.id = 'sweater-description-' + uniqueId;
-
-        domElement.setAttribute( 'aria-describedby', descriptionElement.id );
-
-        // structure the elements
-        domElement.appendChild( labelElement );
-        domElement.appendChild( descriptionElement );
-
-        // build up the correct charge description based on the state of the model
-        var createDescription = function( charge ) {
-          var chargeNeutralityDescriptionString = charge > 0 ? netPositiveString : neutralString;
-
-          var chargeAmountString;
-          if( charge === 0 ) {
-            chargeAmountString = noString;
-          }
-          else if( charge <= 15 ) {
-            chargeAmountString = aFewString;
-          }
-          else if( charge <= 40 && charge > 10 ) {
-            chargeAmountString = severalString;
-          }
-          else if ( charge > 40 ) {
-            chargeAmountString = manyString;
-          }
-          assert && assert( chargeAmountString, 'String charge amount description not defined.' );
-
-          // build the formatted string
-          var chargeDescriptionString = StringUtils.format( sweaterDescriptionPatternString, chargeNeutralityDescriptionString, chargeAmountString );
-
-          // notify if no more charges remain on sweater
-          return charge === 57 ? chargeDescriptionString + ' ' + sweaterNoMoreChargesString : chargeDescriptionString;
-        };
-
-        // whenever the model charge changes, update the accesible description
-        self.sweaterModel.chargeProperty.link( function( charge ) {
-          var chargeDescription = createDescription( charge );
-          if ( descriptionElement.textContent !== chargeDescription ) {
-            descriptionElement.textContent = chargeDescription;
-          }
-        } );
-
-        return new AccessiblePeer( accessibleInstance, domElement );
-
-      }
-    };
   }
 
   balloonsAndStaticElectricity.register( 'SweaterNode', SweaterNode );
 
-  return inherit( Node, SweaterNode );
+  return inherit( AccessibleNode, SweaterNode, {
+
+    /**
+     * Get a description for the sweater, based on its charge.
+     *
+     * @param  {number} charge
+     * @return {string}
+     */
+    getChargeDescription: function( charge ) {
+      var chargeString;
+      var neutralityString;
+
+      if ( charge === 0 ) {
+        chargeString = noString;
+        neutralityString = neutralString;
+      }
+      if ( A_FEW_RANGE.contains( charge ) ) {
+        chargeString = aFewString;
+        neutralityString = positiveString;
+      }
+      else if ( SEVERAL_RANGE.contains( charge ) ) {
+        chargeString = severalString;
+        neutralityString = positiveString;
+      }
+      else if ( MANY_RANGE.contains( charge ) ) {
+        chargeString = manyString;
+        neutralityString = positiveString;
+      }
+      else if ( charge === MAX_CHARGE ){
+
+        // if no more negative charges remain on sweater, return this immediately
+        return sweaterChargeDepletedString;
+      }
+
+      return StringUtils.format( sweaterDescriptionPatternString, neutralityString, chargeString );
+    }
+  } );
 } );
