f43ed22e69a3a8bea30d9d91ac8e628873d8155b
Jesse Greenberg <jesse.greenberg@colorado.edu>
2016-12-22 19:59:04 -0700

    update shift description in keyboard help dialog, see #219

--- js/balloons-and-static-electricity/accessibility/KeyboardHelpDialog.js
+++ js/balloons-and-static-electricity/accessibility/KeyboardHelpDialog.js
@@ -38,11 +38,11 @@ define( function( require ) {
 
   var keysForDraggingAndRubbingString = 'Keys for Dragging and Rubbing';
   var draggindDescriptionString = 'Press the W, A, S, or D keys to drag or rub the balloon up, left, down, or right with small steps.';
-  var pressWString = 'Press W to drag up, or Shift key plus W to drag up a lot.';
-  var pressAString = 'Press A to drag left, or Shift key plus A to drag left a lot. ';
-  var pressSString = 'Press S to drag down, or Shift key plus S to drag down a lot.';
-  var pressDString = 'Press D to drag right, or Shift key plus D to drag right a lot.';
-  var addShiftString = 'Add the Shift key to a letter key to drag or rub with bigger steps.';
+  var pressWString = 'Press W to drag up, or Shift key plus W to drag up a little.';
+  var pressAString = 'Press A to drag left, or Shift key plus A to drag left a little. ';
+  var pressSString = 'Press S to drag down, or Shift key plus S to drag down a little.';
+  var pressDString = 'Press D to drag right, or Shift key plus D to drag right a little.';
+  var addShiftString = 'Add the Shift key to a letter key to drag or rub with smaller steps.';
   var hotkeysJumpingString = 'Hot Keys for Jumping';
   var pressToJumpString = 'Move the balloon quickly to a location with these hot key combinations:';
   var JSString = 'JS jumps close to Sweater.';
