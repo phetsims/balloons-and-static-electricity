bbe72fe3859025b1883f67331937bf37a55a5550
jbphet <john.blanco@colorado.edu>
2017-01-24 17:26:27 -0700

    instrumented AccessibleDragNode, see #204

---
+++ js/simulations/balloons-and-static-electricity/TAccessibleDragNode.js
@@ -0,0 +1,61 @@
+// Copyright 2017, University of Colorado Boulder
+
+/**
+ * wrapper type for AccessibleDragNode
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author John Blanco
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var assertInstanceOf = require( 'PHET_IO/assertions/assertInstanceOf' );
+  var phetioInherit = require( 'PHET_IO/phetioInherit' );
+  var phetioNamespace = require( 'PHET_IO/phetioNamespace' );
+  var TBoolean = require( 'PHET_IO/types/TBoolean' );
+  var TFunctionWrapper = require( 'PHET_IO/types/TFunctionWrapper' );
+  var TNode = require( 'PHET_IO/types/scenery/nodes/TNode' );
+  var toEventOnEmit = require( 'PHET_IO/events/toEventOnEmit' );
+  var TVoid = require( 'PHET_IO/types/TVoid' );
+
+  /**
+   * @param accessibleDragNode
+   * @param phetioID
+   * @constructor
+   */
+  function TAccessibleDragNode( accessibleDragNode, phetioID ) {
+    assertInstanceOf( accessibleDragNode, phet.balloonsAndStaticElectricity.AccessibleDragNode );
+    TNode.call( this, accessibleDragNode, phetioID );
+
+    toEventOnEmit(
+      accessibleDragNode.startedCallbacksForKeyDownEmitter,
+      accessibleDragNode.endedCallbacksForKeyDownEmitter,
+      'user',
+      phetioID,
+      TAccessibleDragNode,
+      'keyDown',
+      function( keyCode ) { return { keyCode: keyCode }; }
+    );
+
+    toEventOnEmit(
+      accessibleDragNode.startedCallbacksForKeyUpEmitter,
+      accessibleDragNode.endedCallbacksForKeyUpEmitter,
+      'user',
+      phetioID,
+      TAccessibleDragNode,
+      'keyUp',
+      function( keyCode ) { return { keyCode: keyCode }; }
+    );
+  }
+
+  phetioInherit( TNode, 'TAccessibleDragNode', TAccessibleDragNode, {}, {
+    documentation: 'A node in the scene graph with representation in the Parallel DOM.  This node can be dragged with the WASD keys.',
+    events: [ 'keyDown', 'keyUp' ]
+  } );
+
+  phetioNamespace.register( 'TAccessibleDragNode', TAccessibleDragNode );
+
+  return TAccessibleDragNode;
+} );
+
