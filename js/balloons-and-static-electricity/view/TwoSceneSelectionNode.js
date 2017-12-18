534235781053e280d8afc432c696fb6626a563f6
samreid <reids@colorado.edu>
2017-12-18 10:44:00 -0700

    Use startEvent/endEvent, see https://github.com/phetsims/tandem/issues/46

--- js/balloons-and-static-electricity/view/TwoSceneSelectionNode.js
+++ js/balloons-and-static-electricity/view/TwoSceneSelectionNode.js
@@ -221,13 +221,13 @@ define( function( require ) {
 
     var downUpListener = new DownUpListener( {
       up: function() {
-        var id = options.tandem.isLegalAndUsable() && phetioEvents.start( 'user', options.tandem.id, TwoSceneSelectionNodeIO, 'fired', {
+        var id = options.tandem.isLegalAndUsable() && self.startEvent( 'user', 'fired', {
           value: property.phetioType.elementType.toStateObject( property.get() === valueA ? valueB : valueA )
         } );
 
         upFunction();
 
-        options.tandem.isLegalAndUsable() && phetioEvents.end( id );
+        options.tandem.isLegalAndUsable() && self.endEvent( id );
       },
       down: function() {
         var otherButton = property.get() === valueA ? bButtonPath : aButtonPath;
