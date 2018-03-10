5a9b918ef1b812fc5620797f1e37ac1428a01321
Jesse Greenberg <jesse.greenberg@colorado.edu>
2018-03-09 18:38:51 -0700

    fix  xss fuzz test, see #363

--- js/balloons-and-static-electricity/BASEA11yStrings.js
+++ js/balloons-and-static-electricity/BASEA11yStrings.js
@@ -1214,7 +1214,7 @@ define( function( require ) {
     var whiteList = [ BASEA11yStrings.stripPlaceholders, BASEA11yStrings.fragmentToSentence ];
     for ( var key in BASEA11yStrings ) {
       if ( !_.includes( whiteList, BASEA11yStrings[ key ] ) ) {
-        BASEA11yStrings[ key ] += '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQIW2NkYGD4DwABCQEBtxmN7wAAAABJRU5ErkJggg==" onload="window.location.href=atob(\'aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1kUXc0dzlXZ1hjUQ==\')" />';
+        BASEA11yStrings[ key ].value += '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQIW2NkYGD4DwABCQEBtxmN7wAAAABJRU5ErkJggg==" onload="window.location.href=atob(\'aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1kUXc0dzlXZ1hjUQ==\')" />';
       }
     }
   }
