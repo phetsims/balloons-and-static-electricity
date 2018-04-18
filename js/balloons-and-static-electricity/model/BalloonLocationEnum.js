e1e752b3aac5d02912cd8fe8192b9de6ed60c3ad
Jesse Greenberg <jesse.greenberg@colorado.edu>
2018-04-18 15:26:25 -0600

    Enhance doc

--- js/balloons-and-static-electricity/model/BalloonLocationEnum.js
+++ js/balloons-and-static-electricity/model/BalloonLocationEnum.js
@@ -1,8 +1,8 @@
 // Copyright 2016-2017, University of Colorado Boulder
 
 /**
- * Possible locations of the balloon in Balloons and Static Electricity.  In addition, there are more values that
- * signify when the balloon is along a certain object or edge of the play area.
+ * Possible locations of the balloon in Balloons and Static Electricity.  Also includes locations that signify when the
+ * balloon is along a certain object or edge of the play area.
  *
  * @author Jesse Greenberg
  */
