6af3901d7f77939b9fb04ba99d9fe7af137ccc36
Jesse Greenberg <jesse.greenberg@colorado.edu>
2018-06-29 13:04:31 -0600

    add line breaks for credits

--- js/balloons-and-static-electricity-main.js
+++ js/balloons-and-static-electricity-main.js
@@ -28,9 +28,9 @@ define( function( require ) {
     var simOptions = {
       credits: {
         leadDesign: 'Noah Podolefsky & Sam Reid',
-        softwareDevelopment: 'Sam Reid, John Blanco, Jesse Greenberg',
-        team: 'Wendy Adams, Trish Loeblein, Emily Moore, Ariel Paul, Kathy Perkins, Taliesin Smith',
-        qualityAssurance: 'Steele Dalton, Bryce Griebenow, Ethan Johnson, Elise Morgan, Liam Mulhall, Oliver Orejola, ' +
+        softwareDevelopment: 'Sam Reid, John Blanco',
+        team: 'Wendy Adams, Jesse Greenberg, Trish Loeblein, Emily Moore, Ariel Paul,<br>Kathy Perkins, Taliesin Smith',
+        qualityAssurance: 'Steele Dalton, Bryce Griebenow, Ethan Johnson, Elise Morgan,<br>Liam Mulhall, Oliver Orejola, ' +
                           'Benjamin Roberts, Jacob Romero, Bryan Yoelin',
         graphicArts: 'Sharon Siman-Tov',
         thanks: 'Thanks to Mobile Learner Labs for working with the PhET development team to convert this simulation to HTML5.'
