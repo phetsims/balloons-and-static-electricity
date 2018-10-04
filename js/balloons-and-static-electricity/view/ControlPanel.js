21c8308cc5eb7c77e67da504f608d6e33623e939
zepumph <michael.kauzmann@colorado.edu>
2018-10-04 13:34:21 -0800

    rename event stream to data stream, https://github.com/phetsims/phet-io/issues/1338

--- js/balloons-and-static-electricity/view/ControlPanel.js
+++ js/balloons-and-static-electricity/view/ControlPanel.js
@@ -213,7 +213,7 @@ define( function( require ) {
       oneBalloonIcon,
       twoBalloonIcon,
       {
-        // TODO: (phet-io event stream) The 'fired' event for this tandem doesn't make sense with a 'value' of boolean in the parameters.
+        // TODO: (phet-io data stream) The 'fired' event for this tandem doesn't make sense with a 'value' of boolean in the parameters.
         tandem: tandem.createTandem( 'showSecondBalloonSelector' ),
         maskFill: BASEConstants.backgroundColorProperty,
         ariaLabel: twoBalloonExperimentLabelString,
