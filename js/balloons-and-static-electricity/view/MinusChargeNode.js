545186a25f847e6aa2774dc217fd90256f3d4ecc
samreid <reids@colorado.edu>
2018-12-12 13:50:26 -0700

    Use Node.rasterized to avoid synchronizing issues, see https://github.com/phetsims/balloons-and-static-electricity/issues/434

--- js/balloons-and-static-electricity/view/MinusChargeNode.js
+++ js/balloons-and-static-electricity/view/MinusChargeNode.js
@@ -11,7 +11,6 @@ define( function( require ) {
   // modules
   var balloonsAndStaticElectricity = require( 'BALLOONS_AND_STATIC_ELECTRICITY/balloonsAndStaticElectricity' );
   var Circle = require( 'SCENERY/nodes/Circle' );
-  var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   var PointChargeModel = require( 'BALLOONS_AND_STATIC_ELECTRICITY/balloons-and-static-electricity/model/PointChargeModel' );
@@ -23,7 +22,7 @@ define( function( require ) {
   //Scale up before rasterization so it won't be too pixellated/fuzzy
   var scale = 2;
 
-  var minusChargeNode = new Node( {
+  const icon = new Node( {
     children: [
       new Circle( RADIUS, {
         x: 0, y: 0,
@@ -38,15 +37,10 @@ define( function( require ) {
         centerX: 0,
         centerY: 0
       } )
-    ], scale: scale
+    ]
   } );
-
-  var node = new Node();
-  minusChargeNode.toImage( function( im ) {
-
-    //Scale back down so the image will be the desired size
-    node.children = [ new Image( im, { scale: 1.0 / scale } ) ];
-  } );
+  icon.translate( icon.width / 2, icon.height / 2 );
+  var sharedMinusChargeNode = icon.rasterized( { resolution: scale } );
 
   /**
    * @constructor
@@ -65,7 +59,7 @@ define( function( require ) {
 
     this.translate( location.x - RADIUS, location.y - RADIUS );
 
-    this.addChild( node );
+    this.addChild( sharedMinusChargeNode );
   }
 
   balloonsAndStaticElectricity.register( 'MinusChargeNode', MinusChargeNode );
--- js/balloons-and-static-electricity/view/PlusChargeNode.js
+++ js/balloons-and-static-electricity/view/PlusChargeNode.js
@@ -11,7 +11,6 @@ define( function( require ) {
   // modules
   var balloonsAndStaticElectricity = require( 'BALLOONS_AND_STATIC_ELECTRICITY/balloonsAndStaticElectricity' );
   var Circle = require( 'SCENERY/nodes/Circle' );
-  var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   var PointChargeModel = require( 'BALLOONS_AND_STATIC_ELECTRICITY/balloons-and-static-electricity/model/PointChargeModel' );
@@ -21,8 +20,8 @@ define( function( require ) {
   var RADIUS = PointChargeModel.RADIUS;
 
   //Scale up before rasterization so it won't be too pixellated/fuzzy
-  var scale = 2;
-  var plusChargeNode = new Node( {
+  var scale = 2; // TODO: factor out: MinusChargeNode also has one
+  const icon = new Node( {
     children: [
       new Circle( RADIUS, {
         x: 0, y: 0,
@@ -43,14 +42,10 @@ define( function( require ) {
         centerX: 0,
         centerY: 0
       } )
-    ], scale: scale
+    ]
   } );
-
-  var node = new Node();
-  plusChargeNode.toImage( function( im ) {
-    //Scale back down so the image will be the desired size
-    node.children = [ new Image( im, { scale: 1.0 / scale } ) ];
-  } );
+  icon.translate( icon.width / 2, icon.height / 2 );
+  var sharedPlusChargeNode = icon.rasterized( { resolution: scale } );
 
   /**
    * @constructor
@@ -63,7 +58,7 @@ define( function( require ) {
 
     this.translate( location.x - RADIUS, location.y - RADIUS );
 
-    this.addChild( node );
+    this.addChild( sharedPlusChargeNode );
   }
 
   balloonsAndStaticElectricity.register( 'PlusChargeNode', PlusChargeNode );
