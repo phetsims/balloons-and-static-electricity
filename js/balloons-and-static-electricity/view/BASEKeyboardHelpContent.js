55cf400262c1cd4eeb771434740f3bbea6636f55
Jesse Greenberg <jesse.greenberg@colorado.edu>
2019-04-06 10:39:02 -0600

    convert to es6

--- js/balloons-and-static-electricity/view/BASEKeyboardHelpContent.js
+++ js/balloons-and-static-electricity/view/BASEKeyboardHelpContent.js
@@ -5,139 +5,144 @@
  *
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var balloonsAndStaticElectricity = require( 'BALLOONS_AND_STATIC_ELECTRICITY/balloonsAndStaticElectricity' );
-  var BASEA11yStrings = require( 'BALLOONS_AND_STATIC_ELECTRICITY/balloons-and-static-electricity/BASEA11yStrings' );
-  var EnterKeyNode = require( 'SCENERY_PHET/keyboard/EnterKeyNode' );
-  var GeneralKeyboardHelpSection = require( 'SCENERY_PHET/keyboard/help/GeneralKeyboardHelpSection' );
-  var HBox = require( 'SCENERY/nodes/HBox' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var KeyboardHelpSection = require( 'SCENERY_PHET/keyboard/help/KeyboardHelpSection' );
-  var LetterKeyNode = require( 'SCENERY_PHET/keyboard/LetterKeyNode' );
-  var SpaceKeyNode = require( 'SCENERY_PHET/keyboard/SpaceKeyNode' );
-  var VBox = require( 'SCENERY/nodes/VBox' );
+  const balloonsAndStaticElectricity = require( 'BALLOONS_AND_STATIC_ELECTRICITY/balloonsAndStaticElectricity' );
+  const BASEA11yStrings = require( 'BALLOONS_AND_STATIC_ELECTRICITY/balloons-and-static-electricity/BASEA11yStrings' );
+  const EnterKeyNode = require( 'SCENERY_PHET/keyboard/EnterKeyNode' );
+  const GeneralKeyboardHelpSection = require( 'SCENERY_PHET/keyboard/help/GeneralKeyboardHelpSection' );
+  const HBox = require( 'SCENERY/nodes/HBox' );
+  const KeyboardHelpSection = require( 'SCENERY_PHET/keyboard/help/KeyboardHelpSection' );
+  const LetterKeyNode = require( 'SCENERY_PHET/keyboard/LetterKeyNode' );
+  const SpaceKeyNode = require( 'SCENERY_PHET/keyboard/SpaceKeyNode' );
+  const VBox = require( 'SCENERY/nodes/VBox' );
 
   // strings
-  var grabOrReleaseBalloonHeadingString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/grabOrReleaseBalloonHeading' );
-  var grabOrReleaseBalloonLabelString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/grabOrReleaseBalloonLabel' );
-  var jumpCloseToSweaterLabelString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/jumpCloseToSweaterLabel' );
-  var jumpCloseToWallLabelString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/jumpCloseToWallLabel' );
-  var jumpNearWallLabelString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/jumpNearWallLabel' );
-  var jumpToCenterLabelString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/jumpToCenterLabel' );
-  var moveGrabbedBalloonLabelString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/moveGrabbedBalloonLabel' );
-  var moveOrJumpGrabbedBalloonHeadingString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/moveOrJumpGrabbedBalloonHeading' );
-  var moveSlowerLabelString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/moveSlowerLabel' );
+  const grabOrReleaseBalloonHeadingString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/grabOrReleaseBalloonHeading' );
+  const grabOrReleaseBalloonLabelString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/grabOrReleaseBalloonLabel' );
+  const jumpCloseToSweaterLabelString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/jumpCloseToSweaterLabel' );
+  const jumpCloseToWallLabelString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/jumpCloseToWallLabel' );
+  const jumpNearWallLabelString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/jumpNearWallLabel' );
+  const jumpToCenterLabelString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/jumpToCenterLabel' );
+  const moveGrabbedBalloonLabelString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/moveGrabbedBalloonLabel' );
+  const moveOrJumpGrabbedBalloonHeadingString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/moveOrJumpGrabbedBalloonHeading' );
+  const moveSlowerLabelString = require( 'string!BALLOONS_AND_STATIC_ELECTRICITY/moveSlowerLabel' );
 
   // a11y strings
-  var grabOrReleaseBalloonDescriptionString = BASEA11yStrings.grabOrReleaseBalloonDescription.value;
-  var moveGrabbedBalloonDescriptionString = BASEA11yStrings.moveGrabbedBalloonDescription.value;
-  var moveSlowerDescriptionString = BASEA11yStrings.moveSlowerDescription.value;
-  var jumpsCloseToSweaterDescriptionString = BASEA11yStrings.jumpsCloseToSweaterDescription.value;
-  var jumpsCloseToWwallDescriptionString = BASEA11yStrings.jumpsCloseToWwallDescription.value;
-  var jumpsNearWallDescriptionString = BASEA11yStrings.jumpsNearWallDescription.value;
-  var jumpstoCenterDescriptionString = BASEA11yStrings.jumpstoCenterDescription.value;
+  const grabOrReleaseBalloonDescriptionString = BASEA11yStrings.grabOrReleaseBalloonDescription.value;
+  const moveGrabbedBalloonDescriptionString = BASEA11yStrings.moveGrabbedBalloonDescription.value;
+  const moveSlowerDescriptionString = BASEA11yStrings.moveSlowerDescription.value;
+  const jumpsCloseToSweaterDescriptionString = BASEA11yStrings.jumpsCloseToSweaterDescription.value;
+  const jumpsCloseToWwallDescriptionString = BASEA11yStrings.jumpsCloseToWwallDescription.value;
+  const jumpsNearWallDescriptionString = BASEA11yStrings.jumpsNearWallDescription.value;
+  const jumpstoCenterDescriptionString = BASEA11yStrings.jumpstoCenterDescription.value;
 
   // constants
   // the english strings are shorter for the balloon help content, so we restrict that content width for i18n more
   // so that the whole content will fit in dev bounds 
-  var BALLOON_CONTENT_MAX_WIDTH = 130;
-  var GENERAL_CONTENT_MAX_WIDTH = 160;
+  const BALLOON_CONTENT_MAX_WIDTH = 130;
+  const GENERAL_CONTENT_MAX_WIDTH = 160;
 
   /**
    * Constructor.
    * @constructor
    */
-  function BASEKeyboardHelpContent() {
+  class BASEKeyboardHelpContent extends HBox {
+    constructor() {
 
-    //  the sections of help content
-    var balloonGrabHelpSection = new BalloonGrabHelpSection( {
-      labelMaxWidth: BALLOON_CONTENT_MAX_WIDTH
-    } );
-    var generalNavigationHelpSection = new GeneralKeyboardHelpSection( {
-      withGroupContent: true,
-      labelMaxWidth: GENERAL_CONTENT_MAX_WIDTH
-    } );
-    var moveBalloonHelpSection = new MoveBalloonHelpSection( {
-      labelMaxWidth: BALLOON_CONTENT_MAX_WIDTH
-    } );
+      //  the sections of help content
+      const balloonGrabHelpSection = new BalloonGrabHelpSection( {
+        labelMaxWidth: BALLOON_CONTENT_MAX_WIDTH
+      } );
+      const generalNavigationHelpSection = new GeneralKeyboardHelpSection( {
+        withGroupContent: true,
+        labelMaxWidth: GENERAL_CONTENT_MAX_WIDTH
+      } );
+      const moveBalloonHelpSection = new MoveBalloonHelpSection( {
+        labelMaxWidth: BALLOON_CONTENT_MAX_WIDTH
+      } );
 
-    // vertically align the left sections
-    KeyboardHelpSection.alignHelpSectionIcons( [ balloonGrabHelpSection, moveBalloonHelpSection ] );
+      // vertically align the left sections
+      KeyboardHelpSection.alignHelpSectionIcons( [ balloonGrabHelpSection, moveBalloonHelpSection ] );
 
-    // sections for how to grab the balloon and general navigation are aligned to the left of the dialog
-    var leftContent = new VBox( {
-      children: [ balloonGrabHelpSection, moveBalloonHelpSection ],
-      align: 'left',
-      spacing: 30
-    } );
+      // sections for how to grab the balloon and general navigation are aligned to the left of the dialog
+      const leftContent = new VBox( {
+        children: [ balloonGrabHelpSection, moveBalloonHelpSection ],
+        align: 'left',
+        spacing: 30
+      } );
 
-    // left aligned sections, and section about how to move the grabbed balloon are horizontally aligned
-    HBox.call( this, {
-      children: [ leftContent, generalNavigationHelpSection ],
-      align: 'top',
-      spacing: 20
-    } );
+      // left aligned sections, and section about how to move the grabbed balloon are horizontally aligned
+      super( {
+        children: [ leftContent, generalNavigationHelpSection ],
+        align: 'top',
+        spacing: 20
+      } );
 
-    // the reading order for screen readers
-    this.accessibleOrder = [ balloonGrabHelpSection, moveBalloonHelpSection, generalNavigationHelpSection ];
+      // the reading order for screen readers
+      this.accessibleOrder = [ balloonGrabHelpSection, moveBalloonHelpSection, generalNavigationHelpSection ];
+    }
   }
 
   balloonsAndStaticElectricity.register( 'BASEKeyboardHelpContent', BASEKeyboardHelpContent );
 
-  inherit( HBox, BASEKeyboardHelpContent );
-
   /**
-   * Help section for how to grab and release the balloon.
-   * @constructor
+   * Inner class. Help section for how to grab and release the balloon.
    */
-  function BalloonGrabHelpSection( options ) {
-    var spaceKeyNode = new SpaceKeyNode();
-    var enterKeyNode = new EnterKeyNode();
-    var icons = KeyboardHelpSection.iconOrIcon( spaceKeyNode, enterKeyNode );
-    var labelWithContent = KeyboardHelpSection.labelWithIcon( grabOrReleaseBalloonLabelString, icons, grabOrReleaseBalloonDescriptionString, {
-      iconOptions: {
-        tagName: 'p' // it is the only item so it is a p rather than an li
-      }
-    } );
+  class BalloonGrabHelpSection extends KeyboardHelpSection {
 
-    KeyboardHelpSection.call( this, grabOrReleaseBalloonHeadingString, [ labelWithContent ], _.extend( {
-      a11yContentTagName: null // just a paragraph for this section, no list
-    }, options ) );
+    /**
+     * @param {Object} options
+     */
+    constructor( options ) {
+      const spaceKeyNode = new SpaceKeyNode();
+      const enterKeyNode = new EnterKeyNode();
+      const icons = KeyboardHelpSection.iconOrIcon( spaceKeyNode, enterKeyNode );
+      const labelWithContent = KeyboardHelpSection.labelWithIcon( grabOrReleaseBalloonLabelString, icons, grabOrReleaseBalloonDescriptionString, {
+        iconOptions: {
+          tagName: 'p' // it is the only item so it is a p rather than an li
+        }
+      } );
+
+      super( grabOrReleaseBalloonHeadingString, [ labelWithContent ], _.extend( {
+        a11yContentTagName: null // just a paragraph for this section, no list
+      }, options ) );
+    }
   }
-
-  inherit( KeyboardHelpSection, BalloonGrabHelpSection );
 
   /**
    * Help section for how to move the balloon or use hot keys to make the balloon jump to locations.
-   * @constructor
    */
-  function MoveBalloonHelpSection( options ) {
+  class MoveBalloonHelpSection extends KeyboardHelpSection  {
 
-    var arrowOrWasdKeysIcon = KeyboardHelpSection.arrowOrWasdKeysRowIcon();
-    var labelWithContent = KeyboardHelpSection.labelWithIcon( moveGrabbedBalloonLabelString, arrowOrWasdKeysIcon, moveGrabbedBalloonDescriptionString );
+    /**
+     * @param {Object} options
+     */
+    constructor( options ) {
 
-    var arrowKeysIcon = KeyboardHelpSection.arrowKeysRowIcon();
-    var shiftAndArrowKeysIcon = KeyboardHelpSection.shiftPlusIcon( arrowKeysIcon );
-    var wasdRowIcon = KeyboardHelpSection.wasdRowIcon();
-    var shiftAndWasdRowIcon = KeyboardHelpSection.shiftPlusIcon( wasdRowIcon );
-    var labelWithIconList = KeyboardHelpSection.labelWithIconList( moveSlowerLabelString, [ shiftAndArrowKeysIcon, shiftAndWasdRowIcon ], moveSlowerDescriptionString );
+      const arrowOrWasdKeysIcon = KeyboardHelpSection.arrowOrWasdKeysRowIcon();
+      const labelWithContent = KeyboardHelpSection.labelWithIcon( moveGrabbedBalloonLabelString, arrowOrWasdKeysIcon, moveGrabbedBalloonDescriptionString );
 
-    // hot key rows for how to jump the balloon
-    var jumpToSweaterRow = createJumpKeyRow( 'S', jumpCloseToSweaterLabelString, jumpsCloseToSweaterDescriptionString );
-    var jumpToWallRow = createJumpKeyRow( 'W', jumpCloseToWallLabelString, jumpsCloseToWwallDescriptionString );
-    var jumpNearWallRow = createJumpKeyRow( 'N', jumpNearWallLabelString, jumpsNearWallDescriptionString );
-    var jumpToCenterRow = createJumpKeyRow( 'C', jumpToCenterLabelString, jumpstoCenterDescriptionString );
+      const arrowKeysIcon = KeyboardHelpSection.arrowKeysRowIcon();
+      const shiftAndArrowKeysIcon = KeyboardHelpSection.shiftPlusIcon( arrowKeysIcon );
+      const wasdRowIcon = KeyboardHelpSection.wasdRowIcon();
+      const shiftAndWasdRowIcon = KeyboardHelpSection.shiftPlusIcon( wasdRowIcon );
+      const labelWithIconList = KeyboardHelpSection.labelWithIconList( moveSlowerLabelString, [ shiftAndArrowKeysIcon, shiftAndWasdRowIcon ], moveSlowerDescriptionString );
 
-    // all rows contained in a left aligned vbox
-    var rows = [ labelWithContent, labelWithIconList, jumpToSweaterRow, jumpToWallRow, jumpNearWallRow, jumpToCenterRow ];
+      // hot key rows for how to jump the balloon
+      const jumpToSweaterRow = createJumpKeyRow( 'S', jumpCloseToSweaterLabelString, jumpsCloseToSweaterDescriptionString );
+      const jumpToWallRow = createJumpKeyRow( 'W', jumpCloseToWallLabelString, jumpsCloseToWwallDescriptionString );
+      const jumpNearWallRow = createJumpKeyRow( 'N', jumpNearWallLabelString, jumpsNearWallDescriptionString );
+      const jumpToCenterRow = createJumpKeyRow( 'C', jumpToCenterLabelString, jumpstoCenterDescriptionString );
 
-    KeyboardHelpSection.call( this, moveOrJumpGrabbedBalloonHeadingString, rows, options );
+      // all rows contained in a left aligned vbox
+      const rows = [ labelWithContent, labelWithIconList, jumpToSweaterRow, jumpToWallRow, jumpNearWallRow, jumpToCenterRow ];
+
+      super( moveOrJumpGrabbedBalloonHeadingString, rows, options );
+    }
   }
-
-  inherit( KeyboardHelpSection, MoveBalloonHelpSection );
 
   /**
    * Create an entry for the dialog that looks horizontally aligns a letter key with a 'J' key separated by a plus
@@ -149,14 +154,14 @@ define( function( require ) {
    * @param {string} innerContent
    * @returns {HBox}
    */
-  function createJumpKeyRow( keyString, labelString, innerContent ) {
+  const createJumpKeyRow = ( keyString, labelString, innerContent ) => {
 
-    var jKey = new LetterKeyNode( 'J' );
-    var otherKey = new LetterKeyNode( keyString );
+    const jKey = new LetterKeyNode( 'J' );
+    const otherKey = new LetterKeyNode( keyString );
 
-    var jPlusOtherKey = KeyboardHelpSection.iconPlusIcon( jKey, otherKey );
+    const jPlusOtherKey = KeyboardHelpSection.iconPlusIcon( jKey, otherKey );
     return KeyboardHelpSection.labelWithIcon( labelString, jPlusOtherKey, innerContent );
-  }
+  };
 
   return BASEKeyboardHelpContent;
 } );
