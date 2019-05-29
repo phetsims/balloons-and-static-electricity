c3456004d81b2051e4bc4375a9a639878f059d2d
zepumph <michael.kauzmann@colorado.edu>
2019-05-29 11:31:41 -0800

    update baseline files, https://github.com/phetsims/phet-io/issues/1466

--- js/phet-io/balloons-and-static-electricity-phet-io-elements-baseline.js
+++ js/phet-io/balloons-and-static-electricity-phet-io-elements-baseline.js
@@ -20882,7 +20882,7 @@ window.phet.phetio.phetioElementsBaseline =
     "balloonsAndStaticElectricity.navigationBar.phetButton.pickableProperty": {
       "phetioDocumentation": "Set whether the phetButton will be pickable (and hence interactive), see the NodeIO documentation for more details",
       "phetioEventType": "model",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
