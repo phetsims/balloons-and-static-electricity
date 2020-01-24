7ebf863e70c52c6a7b7544809a66132de292e40b
phet-dev <phetadmin@gmail.com>
2020-01-24 03:08:00 -0700

    update copyright dates from daily grunt work

--- js/balloons-and-static-electricity/view/BASEShapeHitDetector.js
+++ js/balloons-and-static-electricity/view/BASEShapeHitDetector.js
@@ -1,4 +1,4 @@
-// Copyright 2019, University of Colorado Boulder
+// Copyright 2019-2020, University of Colorado Boulder
 
 /**
  * Detects if a finger is over an element in the view while the user is scanning for objects.
