7c8bc8ca1d1f201084ddcecfeb1412cccf5da05e
zepumph <michael.kauzmann@colorado.edu>
2020-04-02 09:05:30 -0800

    convert namespace from phetIo to phetio, phetio.PhetioIDUtils should use global, https://github.com/phetsims/phet-io/issues/1636

--- js/balloons-and-static-electricity/view/SweaterNode.js
+++ js/balloons-and-static-electricity/view/SweaterNode.js
@@ -125,7 +125,7 @@ function SweaterNode( model, tandem ) {
 
   // When setting the state using phet-io, we must update the charge visibility, otherwise they can get out of sync
   // due to the fact that the movedProperty state could get loaded before the chargeProperty state.
-  _.hasIn( window, 'phet.phetIo.phetioEngine' ) && phet.phetIo.phetioEngine.phetioStateEngine.stateSetEmitter.addListener( function() {
+  _.hasIn( window, 'phet.phetio.phetioEngine' ) && phet.phetio.phetioEngine.phetioStateEngine.stateSetEmitter.addListener( function() {
     updateChargesVisibilityOnSweater( model.showChargesProperty.get() );
   } );
 }
