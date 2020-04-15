003cfc32da26038ef5d9f6757cb263b73e5271b3
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-15 19:30:28 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/balloons-and-static-electricity-baseline.js
+++ js/phet-io/balloons-and-static-electricity-baseline.js
@@ -25689,7 +25689,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "ActionIO<NullableIO<SceneryEventIO>>"
     },
-    "balloonsAndStaticElectricity.general.view.navigationBar.titleTextNode": {
+    "balloonsAndStaticElectricity.general.view.navigationBar.titleText": {
       "phetioDocumentation": "Displays the title of the simulation in the navigation bar (bottom left)",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
@@ -25702,7 +25702,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "TextIO"
     },
-    "balloonsAndStaticElectricity.general.view.navigationBar.titleTextNode.opacityProperty": {
+    "balloonsAndStaticElectricity.general.view.navigationBar.titleText.opacityProperty": {
       "phetioDocumentation": "Opacity of the parent NodeIO, between 0 (invisible) and 1 (fully visible)",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
@@ -25715,7 +25715,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "NumberPropertyIO"
     },
-    "balloonsAndStaticElectricity.general.view.navigationBar.titleTextNode.pickableProperty": {
+    "balloonsAndStaticElectricity.general.view.navigationBar.titleText.pickableProperty": {
       "phetioDocumentation": "Sets whether the node will be pickable (and hence interactive), see the NodeIO documentation for more details",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
@@ -25728,7 +25728,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<NullableIO<BooleanIO>>"
     },
-    "balloonsAndStaticElectricity.general.view.navigationBar.titleTextNode.textProperty": {
+    "balloonsAndStaticElectricity.general.view.navigationBar.titleText.textProperty": {
       "phetioDocumentation": "Property for the displayed text",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
@@ -25741,7 +25741,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<StringIO>"
     },
-    "balloonsAndStaticElectricity.general.view.navigationBar.titleTextNode.visibleProperty": {
+    "balloonsAndStaticElectricity.general.view.navigationBar.titleText.visibleProperty": {
       "phetioDocumentation": "Controls whether the Node will be visible (and interactive), see the NodeIO documentation for more details.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
