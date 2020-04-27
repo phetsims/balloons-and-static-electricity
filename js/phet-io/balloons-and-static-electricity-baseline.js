5e549eee7d30b4e45bc40106d53eb877732e4961
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-27 17:02:00 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/balloons-and-static-electricity-baseline.js
+++ js/phet-io/balloons-and-static-electricity-baseline.js
@@ -25752,7 +25752,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
-    "balloonsAndStaticElectricity.general.view.simNameProperty": {
+    "balloonsAndStaticElectricity.general.model.simNameProperty": {
       "phetioDocumentation": "The name of the sim. Changing this value will update the title text on the navigation bar and the title text on the home screen, if it exists.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
