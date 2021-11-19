4c44cdfebeaa81f375cc737fd8b7d6a8e2f71aee
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:23 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/balloonsAndStaticElectricityStrings.ts
@@ -0,0 +1,38 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import balloonsAndStaticElectricity from './balloonsAndStaticElectricity.js';
+
+type StringsType = {
+  'balloons-and-static-electricity': {
+    'title': string
+  },
+  'BalloonApplet': {
+    'ShowChargeDifferences': string,
+    'ShowAllCharges': string,
+    'ShowNoCharges': string
+  },
+  'removeWall': string,
+  'addWall': string,
+  'resetBalloon': string,
+  'resetBalloons': string,
+  'grabOrReleaseBalloonHeading': string,
+  'grabOrReleaseBalloonLabel': string,
+  'moveOrJumpGrabbedBalloonHeading': string,
+  'moveGrabbedBalloonLabel': string,
+  'moveSlowerLabel': string,
+  'jumpCloseToSweaterLabel': string,
+  'jumpCloseToWallLabel': string,
+  'jumpNearWallLabel': string,
+  'jumpToCenterLabel': string
+};
+
+const balloonsAndStaticElectricityStrings = getStringModule( 'BALLOONS_AND_STATIC_ELECTRICITY' ) as StringsType;
+
+balloonsAndStaticElectricity.register( 'balloonsAndStaticElectricityStrings', balloonsAndStaticElectricityStrings );
+
+export default balloonsAndStaticElectricityStrings;
