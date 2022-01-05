e13d12ea1803824369b6f9b1e7a70ecadc546a1f
phet-dev <phetadmin@gmail.com>
2022-01-05 03:15:45 -0700

    update copyright dates from daily grunt work

--- js/balloons-and-static-electricity/BASEA11yStrings.js
+++ js/balloons-and-static-electricity/BASEA11yStrings.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2021, University of Colorado Boulder
+// Copyright 2017-2022, University of Colorado Boulder
 
 /**
  * Single position of all accessibility strings.  These strings are not meant to be translatable yet.  Rosetta needs
