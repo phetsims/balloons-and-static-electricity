7c00eb6ff968dc8a5123d3e76b04e0091addd266
phet-dev <phetadmin@gmail.com>
2022-01-27 03:22:23 -0700

    update copyright dates from daily grunt work

--- js/balloons-and-static-electricity/view/TwoSceneSelectionNode.js
+++ js/balloons-and-static-electricity/view/TwoSceneSelectionNode.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2021, University of Colorado Boulder
+// Copyright 2016-2022, University of Colorado Boulder
 
 /**
  * Creates a scene selection switch for two scenes.  The visual is styled similar to radio
