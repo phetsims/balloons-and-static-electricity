c866f40e651b4eb165c5634282dbf288e4c1fd7f
phet-dev <phetadmin@gmail.com>
2022-07-21 04:04:32 -0600

    update copyright dates from daily grunt work

--- js/balloons-and-static-electricity/view/PlayAreaGridNode.js
+++ js/balloons-and-static-electricity/view/PlayAreaGridNode.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2021, University of Colorado Boulder
+// Copyright 2016-2022, University of Colorado Boulder
 
 /**
  * Node that shows the various regions of the play area for accessibility.  The play area is broken into
