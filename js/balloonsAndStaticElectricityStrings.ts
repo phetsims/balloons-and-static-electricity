e235b4af872ff22fbb0a2f5a7820171d5972ce07
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:32 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/balloonsAndStaticElectricityStrings.ts
+++ js/balloonsAndStaticElectricityStrings.ts
@@ -5,30 +5,48 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import balloonsAndStaticElectricity from './balloonsAndStaticElectricity.js';
 
 type StringsType = {
   'balloons-and-static-electricity': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'BalloonApplet': {
     'ShowChargeDifferences': string;
+    'ShowChargeDifferencesProperty': TReadOnlyProperty<string>;
     'ShowAllCharges': string;
+    'ShowAllChargesProperty': TReadOnlyProperty<string>;
     'ShowNoCharges': string;
+    'ShowNoChargesProperty': TReadOnlyProperty<string>;
   };
   'removeWall': string;
+  'removeWallProperty': TReadOnlyProperty<string>;
   'addWall': string;
+  'addWallProperty': TReadOnlyProperty<string>;
   'resetBalloon': string;
+  'resetBalloonProperty': TReadOnlyProperty<string>;
   'resetBalloons': string;
+  'resetBalloonsProperty': TReadOnlyProperty<string>;
   'grabOrReleaseBalloonHeading': string;
+  'grabOrReleaseBalloonHeadingProperty': TReadOnlyProperty<string>;
   'grabOrReleaseBalloonLabel': string;
+  'grabOrReleaseBalloonLabelProperty': TReadOnlyProperty<string>;
   'moveOrJumpGrabbedBalloonHeading': string;
+  'moveOrJumpGrabbedBalloonHeadingProperty': TReadOnlyProperty<string>;
   'moveGrabbedBalloonLabel': string;
+  'moveGrabbedBalloonLabelProperty': TReadOnlyProperty<string>;
   'moveSlowerLabel': string;
+  'moveSlowerLabelProperty': TReadOnlyProperty<string>;
   'jumpCloseToSweaterLabel': string;
+  'jumpCloseToSweaterLabelProperty': TReadOnlyProperty<string>;
   'jumpCloseToWallLabel': string;
+  'jumpCloseToWallLabelProperty': TReadOnlyProperty<string>;
   'jumpNearWallLabel': string;
+  'jumpNearWallLabelProperty': TReadOnlyProperty<string>;
   'jumpToCenterLabel': string;
+  'jumpToCenterLabelProperty': TReadOnlyProperty<string>;
 };
 
 const balloonsAndStaticElectricityStrings = getStringModule( 'BALLOONS_AND_STATIC_ELECTRICITY' ) as StringsType;
