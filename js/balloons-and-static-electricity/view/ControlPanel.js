42e10d17ff28cd70d33f382c0eda93379e231e94
phet-dev <phetadmin@gmail.com>
2023-01-17 03:47:35 -0700

    update copyright dates from daily grunt work

--- js/balloons-and-static-electricity/view/ControlPanel.js
+++ js/balloons-and-static-electricity/view/ControlPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2022, University of Colorado Boulder
+// Copyright 2013-2023, University of Colorado Boulder
 
 /**
  * All buttons and control elements for Balloons and Static Electricity.
