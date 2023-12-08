a1154c936ccb1df9145b90e7516361f7ae3aa7b3
Marla Schulz <marlaa.schulz@gmail.com>
2023-12-08 14:21:23 -0700

    Document custom layout bounds across multiple screen views to prevent future breaks in phet-io instrumentation, https://github.com/phetsims/phet-io/issues/1939.

--- js/balloons-and-static-electricity/view/BASEView.js
+++ js/balloons-and-static-electricity/view/BASEView.js
@@ -45,6 +45,10 @@ class BASEView extends ScreenView {
   constructor( model, tandem ) {
 
     super( {
+
+      // A PhET wide decision was made to not update custom layout bounds even if they do not match the
+      // default layout bounds in ScreenView. Do not change these bounds as changes could break or disturb
+      // any phet-io instrumention. https://github.com/phetsims/phet-io/issues/1939
       layoutBounds: new Bounds2( 0, 0, 768, 504 ),
       tandem: tandem
     } );
