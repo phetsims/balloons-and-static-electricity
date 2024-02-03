a95f12789cca3fcc22967ef99e173cf24582d9c3
phet-dev <phetadmin@gmail.com>
2024-02-03 03:14:12 -0700

    update copyright dates from daily grunt work

--- js/balloons-and-static-electricity/view/describers/WallDescriber.js
+++ js/balloons-and-static-electricity/view/describers/WallDescriber.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2021, University of Colorado Boulder
+// Copyright 2017-2024, University of Colorado Boulder
 
 /**
  * A view type that observes the WallModel and builds descriptions which can be read by assistive technology.
