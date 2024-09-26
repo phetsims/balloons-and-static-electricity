d2d5c76f5100a948fd147f308e55c929d944ad50
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:08 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for balloons-and-static-electricity.
+ * ESLint configuration for balloons-and-static-electricity.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
