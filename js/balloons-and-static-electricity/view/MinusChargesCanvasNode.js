10d203b842e0929b57fb0c14b5a8ef9ab6a44394
phet-dev <phetadmin@gmail.com>
2025-02-04 03:29:30 -0700

    update copyright dates from daily grunt work

--- js/balloons-and-static-electricity/view/MinusChargesCanvasNode.js
+++ js/balloons-and-static-electricity/view/MinusChargesCanvasNode.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2021, University of Colorado Boulder
+// Copyright 2018-2025, University of Colorado Boulder
 
 /**
  * A canvas node for minus charges in the wall. This was added as a performance enhancement for #409.
