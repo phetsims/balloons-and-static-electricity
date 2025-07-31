e37cdfe7e2b16cd185fe746a46abf6c32f049084
Sam Reid <reids@colorado.edu>
2025-07-31 15:41:04 -0600

    Convert to TypeScript, see https://github.com/phetsims/balloons-and-static-electricity/issues/577

--- js/balloons-and-static-electricity/view/MinusChargeNode.ts
+++ js/balloons-and-static-electricity/view/MinusChargeNode.ts
@@ -6,9 +6,10 @@
  @author Vasily Shakhov (Mlearner)
  */
 
+import Vector2 from '../../../../dot/js/Vector2.js';
 import merge from '../../../../phet-core/js/merge.js';
 import Circle from '../../../../scenery/js/nodes/Circle.js';
-import Node from '../../../../scenery/js/nodes/Node.js';
+import Node, { NodeOptions } from '../../../../scenery/js/nodes/Node.js';
 import Rectangle from '../../../../scenery/js/nodes/Rectangle.js';
 import RadialGradient from '../../../../scenery/js/util/RadialGradient.js';
 import { rasterizeNode } from '../../../../scenery/js/util/rasterizeNode.js';
@@ -39,17 +40,13 @@ const sharedMinusChargeNode = rasterizeNode( icon, { resolution: BASEConstants.I
 
 class MinusChargeNode extends Node {
 
-  /**
-   * @param {Vector2} position
-   * @param {Object} [options]
-   */
-  constructor( position, options ) {
+  public constructor( position: Vector2, options?: NodeOptions ) {
 
-    options = merge( {
+    const resolvedOptions = merge( {
       pickable: false
     }, options );
 
-    super( options );
+    super( resolvedOptions );
 
     this.translate( position.x + BASEConstants.IMAGE_PADDING, position.y + BASEConstants.IMAGE_PADDING );
 
