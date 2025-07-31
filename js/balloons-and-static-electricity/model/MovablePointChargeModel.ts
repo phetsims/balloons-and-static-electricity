49fcc408f652ea1be5809a06498527456e6b8767
Sam Reid <reids@colorado.edu>
2025-07-31 14:28:51 -0600

    Convert to TypeScript, see https://github.com/phetsims/balloons-and-static-electricity/issues/577

--- js/balloons-and-static-electricity/model/MovablePointChargeModel.ts
+++ js/balloons-and-static-electricity/model/MovablePointChargeModel.ts
@@ -11,22 +11,19 @@
 
 import Vector2 from '../../../../dot/js/Vector2.js';
 import Vector2Property from '../../../../dot/js/Vector2Property.js';
+import Tandem from '../../../../tandem/js/Tandem.js';
 import balloonsAndStaticElectricity from '../../balloonsAndStaticElectricity.js';
 import PointChargeModel from './PointChargeModel.js';
 
 class MovablePointChargeModel extends PointChargeModel {
 
-  /**
-   * @param {number} x
-   * @param {number} y
-   * @param {Tandem} tandem
-   * @param {boolean} phetioState
-   */
-  constructor( x, y, tandem, phetioState ) {
+  // position of the point charge
+  public readonly positionProperty: Vector2Property;
+
+  public constructor( x: number, y: number, tandem: Tandem, phetioState: boolean ) {
 
     super( x, y, tandem, phetioState );
 
-    // @public {Vector2} - position of the point charge
     this.positionProperty = new Vector2Property( this.position, {
       tandem: tandem.createTandem( 'positionProperty' ),
       phetioState: phetioState,
@@ -34,26 +31,18 @@ class MovablePointChargeModel extends PointChargeModel {
     } );
   }
 
-
   /**
    * Reset the point charge.
-   * @public
-   *
-   * @override
    */
-  reset() {
+  public override reset(): void {
     super.reset();
     this.positionProperty.reset();
   }
 
   /**
    * Get the displacement of the charge from its initial position. Useful as a measure of the induced charge.
-   * @public
-   *
-   *
-   * @returns {Vector2}
    */
-  getDisplacement() {
+  public getDisplacement(): number {
     const initialPosition = this.positionProperty.initialValue;
     const displacement = this.positionProperty.get().distance( initialPosition );
 
@@ -62,13 +51,9 @@ class MovablePointChargeModel extends PointChargeModel {
 
   /**
    * Get the center of the charge.
-   * @public
-   *
-   * @returns {Vector2}
-   * @override
    */
-  getCenter() {
-    return new Vector2( this.positionProperty.get().x + this.radius, this.positionProperty.get().y + this.radius );
+  public override getCenter(): Vector2 {
+    return new Vector2( this.positionProperty.get().x + PointChargeModel.RADIUS, this.positionProperty.get().y + PointChargeModel.RADIUS );
   }
 }
 
