ac8367394db979d928d483c0a6718c15cbe058b4
Sam Reid <reids@colorado.edu>
2025-07-31 16:39:33 -0600

    Convert to TypeScript, see https://github.com/phetsims/balloons-and-static-electricity/issues/577

--- js/balloons-and-static-electricity/view/ChargeDeflectionSoundGenerator.ts
+++ js/balloons-and-static-electricity/view/ChargeDeflectionSoundGenerator.ts
@@ -8,11 +8,15 @@
  * @author John Blanco (PhET Interactive Simulations)
  */
 
+import Vector2 from '../../../../dot/js/Vector2.js';
 import phetAudioContext from '../../../../tambo/js/phetAudioContext.js';
 import SoundClip from '../../../../tambo/js/sound-generators/SoundClip.js';
-import SoundGenerator from '../../../../tambo/js/sound-generators/SoundGenerator.js';
+import SoundGenerator, { SoundGeneratorOptions } from '../../../../tambo/js/sound-generators/SoundGenerator.js';
 import chargeDeflection_mp3 from '../../../sounds/chargeDeflection_mp3.js';
 import balloonsAndStaticElectricity from '../../balloonsAndStaticElectricity.js';
+import BalloonModel from '../model/BalloonModel.js';
+import MovablePointChargeModel from '../model/MovablePointChargeModel.js';
+import WallModel from '../model/WallModel.js';
 
 // constants
 const NUMBER_OF_DISCRETE_BINS = 8;
@@ -33,12 +37,31 @@ const PENTATONIC_SCALE_MULTIPLIERS = [
 
 class ChargeDeflectionSoundGenerator extends SoundGenerator {
 
+  // list of original, non-deflected charge positions, used to determine the amount of deflection
+  private readonly originalChargePositions: Vector2[];
+
+  // A list of previous charge positions prior to most recent update.  If using these, be careful to
+  // manage the update process carefully.
+  private previousChargePositions: Vector2[];
+
+  // Bin number, based on normalized deflection, where each charge was mapped at during the
+  // most recent update.
+  private readonly chargeDeflectionBins: number[];
+
+  // sound generators used to produce the individual sounds
+  private readonly soundGenerators: SoundClip[];
+
+  private readonly chargeToSoundGeneratorMap: Map<MovablePointChargeModel, SoundClip>;
+
+  // array of point charges, needed by methods
+  private readonly wallCharges: MovablePointChargeModel[];
+
   /**
-   * @param {WallModel} wall
-   * @param {BalloonModel[]} balloons - the balloons modeled in the sim
-   * @param {Object} [options]
+   * @param wall
+   * @param balloons - the balloons modeled in the sim
+   * @param options
    */
-  constructor( wall, balloons, options ) {
+  public constructor( wall: WallModel, balloons: BalloonModel[], options?: SoundGeneratorOptions ) {
 
     assert && assert( balloons.length === 2, `this assumes 2 balloons, found ${balloons.length}` );
 
@@ -50,15 +73,10 @@ class ChargeDeflectionSoundGenerator extends SoundGenerator {
     // Make sure there aren't more sound generators than charges.
     assert && assert( wallCharges.length >= NUMBER_OF_SOUND_GENERATORS );
 
-    // @private {Vector2[]} - list of original, non-deflected charge positions, used to determine the amount of deflection
     this.originalChargePositions = wallCharges.map( wallCharge => wallCharge.positionProperty.value.copy() );
 
-    // {Vector2[]} - A list of previous charge positions prior to most recent update.  If using these, be careful to
-    // manage the update process carefully.
     this.previousChargePositions = [ ...this.originalChargePositions ];
 
-    // @private {number[]} - Bin number, based on normalized deflection, where each charge was mapped at during the
-    // most recent update.
     this.chargeDeflectionBins = [];
 
     // Create an internal gain stage that will be used to adjust the gain level since multiple inputs are being combined
@@ -66,13 +84,10 @@ class ChargeDeflectionSoundGenerator extends SoundGenerator {
     const outputLevelGainNode = phetAudioContext.createGain();
     outputLevelGainNode.connect( this.mainGainNode );
 
-    // @private {SoundClip[]} - sound generators used to produce the individual sounds
     this.soundGenerators = [];
 
-    // @private {Map.<MovablePointChargeModel,SoundGenerator>}
     this.chargeToSoundGeneratorMap = new Map();
 
-    // @private {MovablePointChargeModel[]} - array of point charges, needed by methods
     this.wallCharges = wallCharges;
 
     // Set the initial bins for each charge.
@@ -98,7 +113,7 @@ class ChargeDeflectionSoundGenerator extends SoundGenerator {
 
     // Monitor the balloons for drift motion (i.e. motion not caused by the user dragging them) and produce sound if
     // this motion causes changes in the positions of the wall charges.
-    balloons.forEach( balloon => {
+    balloons.forEach( ( balloon: BalloonModel ) => {
       balloon.positionProperty.link( () => {
         if ( !balloon.isDraggedProperty.value && balloon.inducingChargeProperty.value ) {
           this.produceSoundForBinChanges( balloon );
@@ -109,11 +124,9 @@ class ChargeDeflectionSoundGenerator extends SoundGenerator {
 
   /**
    * Get a normalized value representing how deflected the specified charge is.
-   * @param {number} chargeIndex - index of the charge withing the internal array
-   * @returns {number}
-   * @private
+   * @param chargeIndex - index of the charge withing the internal array
    */
-  getNormalizedChargeDeflection( chargeIndex ) {
+  private getNormalizedChargeDeflection( chargeIndex: number ): number {
     const deflection =
       this.originalChargePositions[ chargeIndex ].distance( this.wallCharges[ chargeIndex ].positionProperty.value );
 
@@ -127,19 +140,16 @@ class ChargeDeflectionSoundGenerator extends SoundGenerator {
 
   /**
    * Update the previous charge positions to match the current ones.
-   * @private
    */
-  updatePreviousChargePositions() {
+  private updatePreviousChargePositions(): void {
     this.previousChargePositions = this.wallCharges.map( wallCharge => wallCharge.positionProperty.value.copy() );
   }
 
   /**
    * Get the charge that corresponds to the specified sound generator index.
-   * @param {number} soundGeneratorIndex
-   * @returns {number}
-   * @private
+   * @param soundGeneratorIndex
    */
-  getChargeForSoundGeneratorIndex( soundGeneratorIndex ) {
+  private getChargeForSoundGeneratorIndex( soundGeneratorIndex: number ): MovablePointChargeModel {
     assert && assert( soundGeneratorIndex < NUMBER_OF_SOUND_GENERATORS, 'soundGeneratorIndex out of range' );
     const spacing = this.wallCharges.length / ( NUMBER_OF_SOUND_GENERATORS + 1 );
     return this.wallCharges[ Math.floor( ( soundGeneratorIndex + 1 ) * spacing ) ];
@@ -147,10 +157,9 @@ class ChargeDeflectionSoundGenerator extends SoundGenerator {
 
   /**
    * Update the list of bins into which each of the charges falls according to its normalized position.
-   * @private
    */
-  updateChargeDeflectionBins() {
-    this.wallCharges.forEach( ( wallCharge, index ) => {
+  private updateChargeDeflectionBins(): void {
+    this.wallCharges.forEach( ( wallCharge: MovablePointChargeModel, index: number ) => {
       this.chargeDeflectionBins[ index ] = mapNormalizedDeflectionToBin( this.getNormalizedChargeDeflection( index ) );
     } );
   }
@@ -159,10 +168,9 @@ class ChargeDeflectionSoundGenerator extends SoundGenerator {
    * A drag handler method for when a balloon is dragged using a pointer (i.e. a mouse or touch event).  This method
    * determines whether the user's actions have changed the charge positions in the wall in such a way that sound should
    * be produced and, if so, it produces the sound.
-   * @param {BalloonModel} balloon
-   * @public
+   * @param balloon
    */
-  balloonDraggedByPointer( balloon ) {
+  public balloonDraggedByPointer( balloon: BalloonModel ): void {
     this.produceSoundForBinChanges( balloon );
   }
 
@@ -170,9 +178,8 @@ class ChargeDeflectionSoundGenerator extends SoundGenerator {
    * Check all of the charges that have sound generators associated with them and, if the charge has moved to a new
    * bin, produce the corresponding sound.  This also updates the bin positions.
    * @param balloon
-   * @private
    */
-  produceSoundForBinChanges( balloon ) {
+  private produceSoundForBinChanges( balloon: BalloonModel ): void {
 
     // Make a list of the previous charge bins.
     const previousChargeDeflectionBins = [ ...this.chargeDeflectionBins ];
@@ -185,14 +192,14 @@ class ChargeDeflectionSoundGenerator extends SoundGenerator {
 
       const sonifiedCharges = Array.from( this.chargeToSoundGeneratorMap.keys() );
 
-      sonifiedCharges.forEach( charge => {
+      sonifiedCharges.forEach( ( charge: MovablePointChargeModel ) => {
 
         // Get the index for this charge.
         const chargeIndex = this.wallCharges.indexOf( charge );
 
         if ( previousChargeDeflectionBins[ chargeIndex ] !== this.chargeDeflectionBins[ chargeIndex ] ) {
 
-          const soundGenerator = this.chargeToSoundGeneratorMap.get( charge );
+          const soundGenerator = this.chargeToSoundGeneratorMap.get( charge )!;
 
           // The bin changed for the charge associated with this sound generator.  Map the bin to a playback rate and
           // play the sound.
@@ -210,20 +217,18 @@ class ChargeDeflectionSoundGenerator extends SoundGenerator {
    * A drag handler method for when a balloon is dragged using the keyboard or some other non-pointer interaction.  This
    * method determines whether the user's actions have changed the charge positions in the wall in such a way that sound
    * should be produced.
-   * @param {BalloonModel} balloon
-   * @public
+   * @param balloon
    */
-  balloonDraggedByKeyboard( balloon ) {
+  public balloonDraggedByKeyboard( balloon: BalloonModel ): void {
     this.produceSoundForChargeMotion( balloon );
   }
 
   /**
    * Check whether any of the charges in the wall have moved since the last update and, if so, produce a sound that
    * indicates this.  The balloon is checked to make sure that it is actually inducing charge in the wall.
-   * @param {BalloonModel} balloon
-   * @private
+   * @param balloon
    */
-  produceSoundForChargeMotion( balloon ) {
+  private produceSoundForChargeMotion( balloon: BalloonModel ): void {
     this.updateChargeDeflectionBins();
     const currentChargePositions = this.wallCharges.map( wallCharge => wallCharge.positionProperty.value.copy() );
     let playDelay = 0;
@@ -232,11 +237,11 @@ class ChargeDeflectionSoundGenerator extends SoundGenerator {
     if ( balloon.inducingChargeProperty.value ) {
 
       // array to keep track of used playback rates to avoid duplication
-      const usedPlaybackRates = [];
+      const usedPlaybackRates: number[] = [];
 
       // Count how many sound instances are currently being played.
       let totalPlayingSoundClipInstances = this.soundGenerators.reduce(
-        ( totalPlayingInstances, sg ) => sg.getNumberOfPlayingInstances() + totalPlayingInstances,
+        ( totalPlayingInstances: number, sg: SoundClip ) => sg.getNumberOfPlayingInstances() + totalPlayingInstances,
         0
       );
 
@@ -245,14 +250,14 @@ class ChargeDeflectionSoundGenerator extends SoundGenerator {
       // of the sound generators don't get played when lots of movement is occurring, which leads to inconsistent
       // behavior based on where the balloon is.  See
       // https://github.com/phetsims/balloons-and-static-electricity/issues/533.
-      const sortedSonifiedCharges = Array.from( this.chargeToSoundGeneratorMap.keys() ).sort( ( charge1, charge2 ) => {
+      const sortedSonifiedCharges = Array.from( this.chargeToSoundGeneratorMap.keys() ).sort( ( charge1: MovablePointChargeModel, charge2: MovablePointChargeModel ) => {
         const balloonCenter = balloon.getCenter();
         return charge1.position.distance( balloonCenter ) - charge2.position.distance( balloonCenter );
       } );
 
       // For each charge that has an associated sound generator, figure out if that charge has moved since the last drag
       // event and, if so, play a sound.
-      sortedSonifiedCharges.forEach( charge => {
+      sortedSonifiedCharges.forEach( ( charge: MovablePointChargeModel ) => {
 
         // Don't even consider playing a sound if there are already too many playing.  This helps to keep things from
         // sounding too chaotic.
@@ -267,7 +272,7 @@ class ChargeDeflectionSoundGenerator extends SoundGenerator {
 
             // Only play the sound if the same playback rate hasn't already been kicked off on another charge.
             if ( !usedPlaybackRates.includes( playbackRateForBin ) ) {
-              const soundGenerator = this.chargeToSoundGeneratorMap.get( charge );
+              const soundGenerator = this.chargeToSoundGeneratorMap.get( charge )!;
               soundGenerator.setPlaybackRate( playbackRateForBin, 0 );
               soundGenerator.play( playDelay );
               totalPlayingSoundClipInstances += 1;
@@ -283,19 +288,17 @@ class ChargeDeflectionSoundGenerator extends SoundGenerator {
   }
 
   /**
-   * @public
    */
-  reset() {
+  public reset(): void {
     this.previousChargePositions = [ ...this.originalChargePositions ];
   }
 }
 
 /**
  * Get the bin into which the provided normalized deflection value should be mapped.
- * @param {number} normalizedDeflection
- * @returns {number}
+ * @param normalizedDeflection
  */
-const mapNormalizedDeflectionToBin = normalizedDeflection => {
+const mapNormalizedDeflectionToBin = ( normalizedDeflection: number ): number => {
 
   // input parameter checking
   assert && assert( normalizedDeflection >= 0 );
@@ -323,10 +326,9 @@ const mapNormalizedDeflectionToBin = normalizedDeflection => {
 
 /**
  * map bin number to a playback rate for a sound clip
- * @param {number} bin
- * @returns {number}
+ * @param bin
  */
-const mapBinToPlaybackRate = bin => {
+const mapBinToPlaybackRate = ( bin: number ): number => {
   const octave = Math.floor( bin / PENTATONIC_SCALE_MULTIPLIERS.length ) + OCTAVES_OFFSET;
   const index = bin % PENTATONIC_SCALE_MULTIPLIERS.length;
   return PENTATONIC_SCALE_MULTIPLIERS[ index ] * Math.pow( 2, octave );
