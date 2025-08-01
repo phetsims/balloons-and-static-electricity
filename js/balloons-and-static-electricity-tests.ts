3ad6777b13755d61201e564e478ef6c789ac4d51
Sam Reid <reids@colorado.edu>
2025-08-01 08:11:29 -0600

    Convert to TypeScript, see https://github.com/phetsims/balloons-and-static-electricity/issues/577

--- js/balloons-and-static-electricity-tests.ts
+++ js/balloons-and-static-electricity-tests.ts
@@ -12,9 +12,33 @@ import './balloons-and-static-electricity/view/BASESummaryNodeTests.js';
 import './balloons-and-static-electricity/view/describers/BalloonDescriberTests.js';
 import './balloons-and-static-electricity/view/describers/SweaterDescriberTests.js';
 import './balloons-and-static-electricity/view/describers/WallDescriberTests.js';
+import IntentionalAny from '../../phet-core/js/types/IntentionalAny.js';
 
 const iframe = document.createElement( 'iframe' );
 iframe.id = 'testFrame';
+
+// declare that window has type any
+declare const window: {
+  baseModel: IntentionalAny;
+  baseView: IntentionalAny;
+} & IntentionalAny;
+
+// declare phet namespace on iframe's contentWindow
+type PhetWindow = {
+  phet: {
+    joist: {
+      sim: {
+        screens: Array<{
+          model: IntentionalAny;
+          view: IntentionalAny;
+        }>;
+        activeProperty: {
+          set: ( value: boolean ) => void;
+        };
+      };
+    };
+  };
+} & Window;
 
 /**
  * @param event
@@ -27,16 +51,13 @@ const loadListener = ( event: MessageEvent<string>, resolve: () => void ) => {
   const data = JSON.parse( event.data );
 
   if ( data.type === 'load' ) {
-    // @ts-expect-error
     window.removeEventListener( 'message', loadListener );
-    // @ts-expect-error
-    window.baseModel = iframe.contentWindow.phet.joist.sim.screens[ 0 ].model;
-    // @ts-expect-error
-    window.baseView = iframe.contentWindow.phet.joist.sim.screens[ 0 ].view;
+    const phetWindow = iframe.contentWindow as unknown as PhetWindow;
+    window.baseModel = phetWindow.phet.joist.sim.screens[ 0 ].model;
+    window.baseView = phetWindow.phet.joist.sim.screens[ 0 ].view;
 
     // no animation or input for tests, all Properties are controlled directly through access to the model
-    // @ts-expect-error
-    iframe.contentWindow.phet.joist.sim.activeProperty.set( false );
+    phetWindow.phet.joist.sim.activeProperty.set( false );
 
     resolve();
   }
@@ -50,7 +71,7 @@ qunitStart();
 // receive the 'load' message from the sim, indicating that model and view are fully constructed and usable in tests
 QUnit.begin( () => {
   return new Promise( ( resolve, reject ) => {
-    window.addEventListener( 'message', event => {
+    window.addEventListener( 'message', ( event: MessageEvent<string> ) => {
       loadListener( event, resolve );
     } );
 
@@ -59,9 +80,7 @@ QUnit.begin( () => {
 } );
 
 QUnit.done( () => {
-  // @ts-expect-error
   delete window.baseModel;
-  // @ts-expect-error
   delete window.baseView;
 
   // @ts-expect-error
